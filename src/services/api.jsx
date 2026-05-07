c94e6fbc5cb44aed1f74632bfbd100feba0084d6
Ankush-Kumar-Cloud <ankushkumary77@gmail.com>
2026-05-07 21:59:33 +0530

    fix api base url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://full-stack-task-backend.onrender.com",
+  baseURL:
+    "https://full-stack-task-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
